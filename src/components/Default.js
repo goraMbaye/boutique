4c3eb75419f4b5785bc783add25bfae04e739678
goraMbaye <g.mbaye3182@zig.univ.sn>
2020-05-15 21:10:22 +0400

    first commit

---
+++ src/components/Default.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class  extends Component {
+    render() {
+        return (
+            <div>
+                <h3>page not found</h3>
+            </div>
+        )
+    }
+}
---
+++ src/data.js
@@ -0,0 +1,110 @@
+export const storeProducts=[
+{
+    id:1,
+    title:"Google Pixel -Black" ,
+    img: "img/product-1.jpg",
+    price:"10",
+    company:"GOOGLE",
+    info:
+    "lorem isa doctor who live in senegal ",
+    inCart: false,
+    count:0,
+    totat:0
+},
+{
+    id:2,
+    title:"Google Pixel -Black" ,
+    img: "img/product-2.jpg",
+    price:"10",
+    company:"GOOGLE",
+    info:
+    "lorem isa doctor who live in senegal ",
+    inCart: false,
+    count:0,
+    totat:0
+},
+{
+    id:3,
+    title:"Google Pixel -Black" ,
+    img: "img/product-3.jpg",
+    price:"10",
+    company:"GOOGLE",
+    info:
+    "lorem isa doctor who live in senegal ",
+    inCart: false,
+    count:0,
+    totat:0
+},
+{
+    id:4,
+    title:"Google Pixel -Black" ,
+    img: "img/product-4.jpg",
+    price:"10",
+    company:"GOOGLE",
+    info:
+    "lorem isa doctor who live in senegal ",
+    inCart: false,
+    count:0,
+    totat:0
+},
+{
+    id:5,
+    title:"Google Pixel -Black" ,
+    img: "img/product-5.jpg",
+    price:"10",
+    company:"GOOGLE",
+    info:
+    "lorem isa doctor who live in senegal ",
+    inCart: false,
+    count:0,
+    totat:0
+},
+{
+    id:6,
+    title:"Google Pixel -Black" ,
+    img: "img/product-6.jpg",
+    price:"10",
+    company:"GOOGLE",
+    info:
+    "lorem isa doctor who live in senegal ",
+    inCart: false,
+    count:0,
+    totat:0
+},
+{
+    id:7,
+    title:"Google Pixel -Black" ,
+    img: "img/product-7.jpg",
+    price:"10",
+    company:"GOOGLE",
+    info:
+    "lorem isa doctor who live in senegal ",
+    inCart: false,
+    count:0,
+    totat:0
+},
+{
+    id:8,
+    title:"Google Pixel -Black" ,
+    img: "img/product-8.jpg",
+    price:"100000",
+    company:"GOOGLE",
+    info:
+    "lorem isa doctor who live in senegal ",
+    inCart: false,
+    count:0,
+    totat:0
+},
+]
+export const detailProduct={
+    id:1,
+    title:"Google Pixel -Black" ,
+    img: "img/product-1.jpg",
+    price:"10",
+    company:"GOOGLE",
+    info:
+    "lorem isa doctor who live in senegal ",
+    inCart: false,
+    count:0,
+    totat:0
+} ;
