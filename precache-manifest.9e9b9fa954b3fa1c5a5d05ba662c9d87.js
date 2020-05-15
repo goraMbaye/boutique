57b64a75352ff0b999aafe360a261f1f09851ad7
goraMbaye <g.mbaye3182@zig.univ.sn>
2020-05-15 21:44:25 +0400

    Updates

---
+++ precache-manifest.9e9b9fa954b3fa1c5a5d05ba662c9d87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8be33b0db449778b1328cf7ef2e02ff",
+    "url": "/boutique/index.html"
+  },
+  {
+    "revision": "f6a34765cd89a6ec9cc3",
+    "url": "/boutique/static/css/2.2cbafada.chunk.css"
+  },
+  {
+    "revision": "bf561176346a980accf6",
+    "url": "/boutique/static/css/main.69e3fac7.chunk.css"
+  },
+  {
+    "revision": "f6a34765cd89a6ec9cc3",
+    "url": "/boutique/static/js/2.be3e3bb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/boutique/static/js/2.be3e3bb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf561176346a980accf6",
+    "url": "/boutique/static/js/main.8e1d1a75.chunk.js"
+  },
+  {
+    "revision": "cadddc86bdbc75b1b1a6",
+    "url": "/boutique/static/js/runtime-main.464932b8.js"
+  }
+]);
